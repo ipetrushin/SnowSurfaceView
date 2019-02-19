bcd9f4accd9c6be2c6011c570c635d4f068023ec
Ivan Petrushin <ivan.kiel@gmail.com>
2019-02-19 11:36:17 +0800

    stopping thread added

--- app/src/main/java/com/example/student/gcard/SnowSurfaceView.java
+++ app/src/main/java/com/example/student/gcard/SnowSurfaceView.java
@@ -10,14 +10,18 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class SnowSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+
+    DrawThread thread;
+    int r =0, g = 0, b = 0;
     public SnowSurfaceView(Context context, AttributeSet attrs) {
         super(context, attrs);
         getHolder().addCallback(this);
     }
 
     class DrawThread extends  Thread {
+        boolean isRunning = true;
         SurfaceHolder holder;
-        public DrawThread(SurfaceHolder holder) {
+        DrawThread(SurfaceHolder holder) {
             super();
             this.holder = holder;
 
@@ -25,26 +29,41 @@ public class SnowSurfaceView extends SurfaceView implements SurfaceHolder.Callba
 
         @Override
         public void run() {
-            Canvas c = holder.lockCanvas();
-            c.drawColor(Color.MAGENTA);
-            holder.unlockCanvasAndPost(c);
+            while (isRunning) {
+                r += 10;
+                if (r > 255) {
+                    r = 0;
+                    g += 20;
+                }
+                if (g > 255) {
+                    g = 0;
+                    b += 40;
+                }
+                try {
+                    sleep(50);
+                } catch (InterruptedException e) {}
+
+                Canvas c = holder.lockCanvas();
+                c.drawColor(Color.rgb(r,g,b));
+                holder.unlockCanvasAndPost(c);
+            }
         }
     }
 
 
     @Override
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
-        DrawThread thread = new DrawThread(surfaceHolder);
+        thread = new DrawThread(surfaceHolder);
         thread.start();
     }
 
     @Override
     public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
-
+        thread.isRunning = false;
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-
+        thread.isRunning = false;
     }
 }
