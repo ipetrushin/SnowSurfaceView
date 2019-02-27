3834860d1e6b87f9fb14294d02e1746a7be7529d
Ivan Petrushin <ivan.kiel@gmail.com>
2019-02-27 09:54:52 +0800

    добалена обработка нажатий

--- app/src/main/java/com/example/student/gcard/SnowSurfaceView.java
+++ app/src/main/java/com/example/student/gcard/SnowSurfaceView.java
@@ -4,48 +4,65 @@ package com.example.student.gcard;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class SnowSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
 
     DrawThread thread;
-    int r =0, g = 0, b = 0;
+    
+    // центр объекта, например, круга
+    float center_x = 0, center_y = 0;
     public SnowSurfaceView(Context context, AttributeSet attrs) {
         super(context, attrs);
         getHolder().addCallback(this);
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float tx = event.getX();
+        float ty = event.getY();
+
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            center_x = tx;
+            center_y = ty;
+        }
+        else  {
+
+        }
+        return true;
+    }
+
     class DrawThread extends  Thread {
-        boolean isRunning = true;
+        boolean isRunning = true; // флаг для управления потоком
         SurfaceHolder holder;
+        Paint p; // объект-кисть
         DrawThread(SurfaceHolder holder) {
             super();
             this.holder = holder;
-
+            p = new Paint();
         }
 
         @Override
         public void run() {
             while (isRunning) {
-                r += 10;
-                if (r > 255) {
-                    r = 0;
-                    g += 20;
-                }
-                if (g > 255) {
-                    g = 0;
-                    b += 40;
-                }
+
                 try {
                     sleep(50);
                 } catch (InterruptedException e) {}
 
                 Canvas c = holder.lockCanvas();
-                c.drawColor(Color.rgb(r,g,b));
-                holder.unlockCanvasAndPost(c);
+                // проверить, существует ли канва
+                if (c != null) {
+                    c.drawColor(Color.BLACK);
+                    p.setColor(Color.YELLOW);
+                    c.drawCircle(center_x, center_y, 20, p);
+                    holder.unlockCanvasAndPost(c);
+                }
             }
         }
     }
@@ -59,11 +76,17 @@ public class SnowSurfaceView extends SurfaceView implements SurfaceHolder.Callba
 
     @Override
     public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+        // при изменении поверхности перезапустить поток
         thread.isRunning = false;
+        if (surfaceHolder != null) {
+            thread = new DrawThread(surfaceHolder);
+            thread.start();
+        }
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        // остановить поток, если поверхности больше нет
         thread.isRunning = false;
     }
 }
